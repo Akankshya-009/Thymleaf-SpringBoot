1d5140e70278e7c758e379028bed40bb8a5e9c58
Akankshya Das <dasmama73809@gmail.com>
2023-11-23 20:03:00 +0530

    Springboot

---
+++ src/test/java/org/jt/thymleafconcept/ThymleafconceptApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jt.thymleafconcept;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymleafconceptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
